c5280263675e7405b9cd46fbde52504b28cda437
menamilochomil <jimenamilochomil@gmail.com>
2022-10-17 12:14:51 -0500

    feat: adds read file fucntion in index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+module.exports = (doc) => {
+  fs.readFile(doc, "utf8", function (err, data) {
+    if(err) {
+      return console.log(err);
+    }
+      return console.log(data);
+  });
 };
