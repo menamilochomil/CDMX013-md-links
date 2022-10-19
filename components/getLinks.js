71bfab2e9447786e4784463c547428afee05c77c
menamilochomil <jimenamilochomil@gmail.com>
2022-10-18 19:33:18 -0500

    feat: adds a syncronus function to get links and display on the terminal in getLinks.js

---
+++ components/getLinks.js
@@ -0,0 +1,28 @@
+//Require filesystem module to read the files
+const fs = require('fs');
+// const path = require('path');
+
+//Require marked and cheerio library to transform the file into a HTML and manipulate the DOM
+const cheerio = require('cheerio');
+const marked = require('marked');
+
+module.exports = (doc) => {
+let arrLinks =[]
+let countLinks = 0
+
+const data = fs.readFileSync(doc, 'utf8')
+const dataHTML = marked.parse(data)
+const $ = cheerio.load(dataHTML);
+
+$('a').each((i, link) => {
+    arrLinks[i]= {
+        href: link.attribs.href,
+        text: $(link).text(),
+        file: doc
+    }
+    countLinks ++
+})
+
+console.log(arrLinks, `
+      We have ${countLinks} links here`)
+  };
