d25a9cc129f76b62e6b87d492a4db907dcd8ad79
menamilochomil <jimenamilochomil@gmail.com>
2022-10-18 19:29:50 -0500

    config: rewrites the path of the function to get more order in cli.js

--- cli.js
+++ cli.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
-const myModule = require("./index.js");
+const myModule = require("./components/getLinks.js");
 let doc = process.argv[2];
 
+
 myModule(doc)
+// let directory = process.argv[2]  
+// let ext = process.argv[3];
+
+// myModule(directory, ext, function(err, files) {
+//     if (err) {
+//         return console.error('error:', err);
+//     }
+    
+//     files.forEach(function(file) {
+//         console.log(file);
+//     });
+// });
+// // C:/Users/jimel/OneDrive/Escritorio/Laboratoria
