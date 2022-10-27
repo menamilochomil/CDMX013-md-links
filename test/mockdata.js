d995f4568b3b6b9a477e7bcd06ea63bd6ff3ba25
menamilochomil <jimenamilochomil@gmail.com>
2022-10-27 16:05:58 -0500

    test: adds mock data in test file

---
+++ test/mockdata.js
@@ -0,0 +1,22 @@
+const arr = [
+    {
+href: "https://es.wikipedia.org/wiki/Markdown",
+text: "Markdown",
+file: "./md/x.md"
+    }
+]
+const arrStats = [
+    {
+href: "https://es.wikipedia.org/wiki/Markdown",
+text: "Markdown",
+file: "./md/x.md",
+status: 400,
+statusText: "ok"
+    }
+]
+
+
+module.exports = {
+arr,
+arrStats
+}
