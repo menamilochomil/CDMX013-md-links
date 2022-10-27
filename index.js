5a1a3a03d6969803755ffebc0d30a59c637a1616
menamilochomil <jimenamilochomil@gmail.com>
2022-10-27 14:56:34 -0500

    feat: adds mdlinks funtion in index.js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const getLinks = require("./components/getLinks.js");
 const validateLinks = require("./components/validateLinks.js");
 const getStats = require("./components/getStats.js");
-const getBroken = require("./components/getBrokenLinks.js");
+// const getBroken = require("./components/getBrokenLinks.js");
 
 const mdLinks = (path, options) => {
   return new Promise (function (resolve, reject) {
-    if(options.validate){
+    if(options.validate === true){
       resolve (validateLinks(getLinks(path)).then(console.log))
     }
-    if(!options.validate){
+    else if (options.validate === false){
       resolve(console.log(getLinks(path)))
     } 
-    if(options.stats){
-      resolve (getStats(getLinks(path)).then(console.log))
+    else if (options.stats === true){
+      let arr = validateLinks(getLinks(path))
+      resolve  (getStats(arr))
     }
-  }
+    else{
+      reject()
+    }
+  }  
   )
 }
 
-module.exports = {
-  mdLinks
-}
+// module.exports = {
+//   mdLinks
+// }
 
-// mdLinks("./md/x.md", { stats: true})
+mdLinks("./md/x.md", { validate: true})
