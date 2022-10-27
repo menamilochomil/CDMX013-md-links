d68e7142a5788750bf382fe0c858b13bdbe2a7aa
menamilochomil <jimenamilochomil@gmail.com>
2022-10-27 16:00:20 -0500

    test: adds two test in md-links.spec.js

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,24 @@
-const mdLinks = require('../');
-
-
+const validateLinks = require('../components/validateLinks.js');
+const {arr, arrStats} = require('./mockdata.js');
+// jest.mock("node-fetch")
+// const {enableFetchMocks} = require('jest-fetch-mock')
+// enableFetchMocks(
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  test('should a function', () => {
+    expect(typeof validateLinks).toBe('function');
+    expect(typeof validateLinks).not.toBe('string');
   });
 
+// trabajar el test como una promesa 
+//No es necesario usar una biblioteca para mockear fetch
+//mockear fetch como createaccount o alguna otra función de firebase
+// 
+  test('should a --validate', () => {
+    return (validateLinks(arr)).then((results) => {
+      expect(results).toEqual(arrStats)
+    })
+  });
 });
+
+  
