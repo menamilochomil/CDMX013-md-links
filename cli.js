439fdfb86e408b86ed9bc07a4ddb33839959000d
menamilochomil <jimenamilochomil@gmail.com>
2022-10-26 19:52:06 -0500

    fixBug: adds the conditional in to a function cli.js

--- cli.js
+++ cli.js
@@ -6,52 +6,30 @@ const getBroken = require("./components/getBrokenLinks.js");
 const args = process.argv;
 const path = process.argv[2];
 
-//  let arr = getLinks(path);
-// // let arr2 = ["oK", "OK"]
-// // let arr2 = arr.map((i)=> i.href)
-
-//Cases
-/*
-function cli (path, option) {
-    if(!option[3]){
-        console.log(getLinks(path))
-     }
-     
-    if (option[3] === "--validate"){
-       validateLinks(getLinks(path))
-    }
-    
-    if(option[3] === "--stats"){
-        console.log("aiuda")
-        // console.log(getStats(validateLinks(getLinks(path))))
-    } 
-    if(option[3] === "--stats" && option[4] === "--validate"){
-        console.log({
-            "total": 3,
-            "Unique": 3,
-            "broken": 1
-        })
-    }
-}
-
-cli(path, option)
-*/
-
-if(args[3] === undefined){
-    console.log(getLinks(path))
+const cli = (path, option) => {
+ if(option[3] === undefined){
+    getLinks(path).map((item)=>{
+    console.log(`
+href: ${item.href}
+text: ${item.text}
+file: ${item.file}
+        `)
+       })
  }
- 
-if (args[3] === "--validate"){
-   validateLinks(getLinks(path)).then(console.log)
-}
+    
+ if (option[3] === "--validate"){
+    validateLinks(getLinks(path)).then((res) => res.map((item) =>
+    {console.log(`${item.file} ${item.href} ${item.statusText} ${item.status} ${item.text}`)
+    }))
+ }
 
-if(args[3] === "--stats" && args[4] === undefined){
-    // console.log("aiuda")
+ if(option[3] === "--stats" && option[4] === undefined){
     getStats(validateLinks(getLinks(path)))
-} 
-if(args[3] === "--stats" && args[4] === "--validate"){
-    getBroken(validateLinks(getLinks(path)))
-}
-// validateLinks(getLinks(path)).then(console.log)
+ } 
 
-// getStats(validateLinks(getLinks(path)))
+ if(option[3] === "--stats" && option[4] === "--validate"){
+    getBroken(validateLinks(getLinks(path)))
+ }
+}
+
+cli(path, args)
