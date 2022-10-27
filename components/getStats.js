c9fd1b100bf3465cdc112f40a8709a0bdc4bb912
menamilochomil <jimenamilochomil@gmail.com>
2022-10-26 19:09:18 -0500

    refact: adds template strings in stats.js

--- components/getStats.js
+++ components/getStats.js
@@ -2,12 +2,11 @@ module.exports = (links) => {
     links.then((res) => {
         const arr = res.map((obj) => obj.href);
         const setU = new Set (arr);
-        const brokenLinks = res.filter((obj) => obj.statusText === "fail");
-        const data = {
-            Total: arr.length,
-            Unique: setU.size,
-            Broken: brokenLinks.length
-        }
+        // const brokenLinks = res.filter((obj) => obj.statusText === "fail");
+        const data = `
+        Total: ${arr.length}
+        Unique: ${setU.size}
+            `
         //Debe ser return
         console.log(data)
     })
