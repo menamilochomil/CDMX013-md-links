4eba332494f02b20aff8d4a7a747009c8f4cbf2e
menamilochomil <jimenamilochomil@gmail.com>
2022-10-26 19:14:48 -0500

    feat: adds conditional as 1st step to use CLI with bin in cli.js

--- cli.js
+++ cli.js
@@ -1,21 +1,57 @@
 #!/usr/bin/env node
 const getLinks = require("./components/getLinks.js");
 const validateLinks = require("./components/validateLinks.js");
-const doc = process.argv[2];
+const getStats = require("./components/getStats.js");
+const getBroken = require("./components/getBrokenLinks.js");
+const args = process.argv;
+const path = process.argv[2];
 
-let arr = getLinks(doc)
-// let arr2 = arr.map((i)=> i.href)
-validateLinks(arr, doc, `holis`)
-// let directory = process.argv[2]  
-// let ext = process.argv[3];
+//  let arr = getLinks(path);
+// // let arr2 = ["oK", "OK"]
+// // let arr2 = arr.map((i)=> i.href)
 
-// myModule(directory, ext, function(err, files) {
-//     if (err) {
-//         return console.error('error:', err);
-//     }
+//Cases
+/*
+function cli (path, option) {
+    if(!option[3]){
+        console.log(getLinks(path))
+     }
+     
+    if (option[3] === "--validate"){
+       validateLinks(getLinks(path))
+    }
     
-//     files.forEach(function(file) {
-//         console.log(file);
-//     });
-// });
-// // C:/Users/jimel/OneDrive/Escritorio/Laboratoria
+    if(option[3] === "--stats"){
+        console.log("aiuda")
+        // console.log(getStats(validateLinks(getLinks(path))))
+    } 
+    if(option[3] === "--stats" && option[4] === "--validate"){
+        console.log({
+            "total": 3,
+            "Unique": 3,
+            "broken": 1
+        })
+    }
+}
+
+cli(path, option)
+*/
+
+if(args[3] === undefined){
+    console.log(getLinks(path))
+ }
+ 
+if (args[3] === "--validate"){
+   validateLinks(getLinks(path)).then(console.log)
+}
+
+if(args[3] === "--stats" && args[4] === undefined){
+    // console.log("aiuda")
+    getStats(validateLinks(getLinks(path)))
+} 
+if(args[3] === "--stats" && args[4] === "--validate"){
+    getBroken(validateLinks(getLinks(path)))
+}
+// validateLinks(getLinks(path)).then(console.log)
+
+// getStats(validateLinks(getLinks(path)))
