0d4e2320e14e1b970a864ea6a51bff8c2a9f1123
menamilochomil <jimenamilochomil@gmail.com>
2022-10-28 11:18:47 -0500

    test: adds two more test in .spec.js

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,9 +1,11 @@
 const validateLinks = require('../components/validateLinks.js');
-const {arr, arrStats} = require('./mockdata.js');
+const getStats = require("../components/getStats.js");
+const getBroken = require("../components/getBrokenLinks.js");
+const {arr, arrStats, arrFail, arrStatsFail, stats} = require('./mockdata.js');
 // jest.mock("node-fetch")
 describe('mdLinks', () => {
 
-  test('should a function', () => {
+  test('validateLinks should be a function', () => {
     expect(typeof validateLinks).toBe('function');
     expect(typeof validateLinks).not.toBe('string');
   });
@@ -12,10 +14,32 @@ describe('mdLinks', () => {
 //No es necesario usar una biblioteca para mockear fetch
 //mockear fetch como createaccount o alguna otra función de firebase
 // 
-  test('should a --validate', () => {
+  test('validateLinks should resolves an array with status 200', () => {
     return (validateLinks(arr)).then((results) => {
       expect(results).toEqual(arrStats)
     })
+  });
+
+  test('validateLinks should resolves an array with status different to 200', () => {
+    return (validateLinks(arrFail)).then((results) => {
+      expect(results).not.toEqual(arrStatsFail)
+    })
+  });
+
+  test('getStats should be a function', () => {
+    expect(typeof getStats).toBe('function');
+    expect(typeof getStats).not.toBe('string');
+  });
+
+  // test('getStats should show info', () => {
+  //   return (getStats(arrStats)).then((results) => {
+  //     expect(results).toEqual(arrStats)
+  //   })
+  // });
+
+  test('getBroken should be a function', () => {
+    expect(typeof getBroken).toBe('function');
+    expect(typeof getBroken).not.toBe('string');
   });
 });
 
