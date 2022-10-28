889f8c8fa0cf39c20d3d9d62b25719876f36726f
menamilochomil <jimenamilochomil@gmail.com>
2022-10-28 11:19:28 -0500

    test: adds more data for testing in mock.js

--- test/mockdata.js
+++ test/mockdata.js
@@ -10,13 +10,34 @@ const arrStats = [
 href: "https://es.wikipedia.org/wiki/Markdown",
 text: "Markdown",
 file: "./md/x.md",
-status: 400,
+status: 200,
 statusText: "ok"
     }
 ]
 
+const arrFail = [
+    {
+        href: "https://www.gog.com/error/404",
+        text: "https://www.gog.com/error/404",
+        file: "./md/x.md"
+    }
+]
+const arrStatsFail = [
+    {
+href: "https://www.gog.com/error/404",
+text: "https://www.gog.com/error/404",
+file: "./md/x.md",
+status: 404,
+statusText: "fail"
+    }
+]
+
+const stats = "./md/x.md https://es.wikipedia.org/wiki/Markdown ok 200 Markdown"
 
 module.exports = {
 arr,
-arrStats
+arrStats,
+arrFail,
+arrStatsFail,
+stats
 }
