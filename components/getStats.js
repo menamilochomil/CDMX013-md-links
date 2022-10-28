7340c31d62505ae5711b485551607a47338f50be
menamilochomil <jimenamilochomil@gmail.com>
2022-10-27 20:01:38 -0500

    feat: adds new info for the users in stats.js

--- components/getStats.js
+++ components/getStats.js
@@ -1,13 +1,29 @@
 module.exports = (links) => {
     links.then((res) => {
-        const arr = res.map((obj) => obj.href);
-        const setU = new Set (arr);
-        // const brokenLinks = res.filter((obj) => obj.statusText === "fail");
-        const data = `
-        Total: ${arr.length}
-        Unique: ${setU.size}
-            `
-        //Debe ser return
+    // Gets the array of validate links with href
+        const arrLinks = res.map((obj) => obj.href);
+    //Gets the unique links
+        const setUnique = [...new Set (arrLinks)];
+        let uniqueLinks = "";
+        const uniqueFor = setUnique.forEach((value) => {
+            uniqueLinks += value + "\n";
+          })
+    //Gets the repeat links
+        const repeatLinks = arrLinks.filter((elemento, index) => {
+            return arrLinks.indexOf(elemento) !== index;
+        });
+        
+        const data = 
+        `Total: ${arrLinks.length}
+
+Unique: ${setUnique.length}
+List of unique links:
+${uniqueLinks}
+
+Repeat: ${repeatLinks.length}
+List of repeat links:
+${String(repeatLinks).replace(/,/g, "\n")}`
+
         console.log(data)
     })
 }
