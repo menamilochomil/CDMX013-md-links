54128cda3398b6feb97bce5412d0b137751479df
menamilochomil <jimenamilochomil@gmail.com>
2022-10-27 19:08:52 -0500

    bugfix: sorts the conditional to get the rigth results in index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const getLinks = require("./components/getLinks.js");
 const validateLinks = require("./components/validateLinks.js");
 const getStats = require("./components/getStats.js");
-// const getBroken = require("./components/getBrokenLinks.js");
+const getBroken = require("./components/getBrokenLinks.js");
 
 const mdLinks = (path, options) => {
   return new Promise (function (resolve, reject) {
-    if(options.validate === true){
-      resolve (validateLinks(getLinks(path)).then(console.log))
+    if (options.stats === true && options.validate === true){
+      resolve (getBroken(validateLinks(getLinks(path))))
+    }
+    else if(options.validate === true){
+      resolve (validateLinks(getLinks(path)))
+    }
+    else if (options.stats === true ){
+      resolve  (getStats(validateLinks(getLinks(path))))
     }
     else if (options.validate === false){
-      resolve(console.log(getLinks(path)))
-    } 
-    else if (options.stats === true){
-      let arr = validateLinks(getLinks(path))
-      resolve  (getStats(arr))
-    }
+      resolve (getLinks(path))
+    }    
     else{
       reject()
     }
-  }  
-  )
+  })
 }
 
-// module.exports = {
-//   mdLinks
-// }
+module.exports = {
+  mdLinks
+}
 
-mdLinks("./md/x.md", { validate: true})
+// mdLinks("./md/x.md", { validate: true}).then(console.log)
